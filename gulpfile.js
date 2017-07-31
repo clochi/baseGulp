cf04d66f6ceced03c261604c9b12686dd39394ac
Claudio Zurita <claudiozurita87@gmail.com>
2017-07-31 15:23:03 -0300

    Se agregó para components

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,32 @@ uglify = require('gulp-uglify');
 htmlMin = require('gulp-htmlmin');
 
 gulp.task('html', function(){
-  gulp.src('src/*.html')
+  gulp.src('src/**/*.html')
     .pipe(htmlMin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'));
 });
+
+//gulp.task('componentsHtml', function(){
+//  gulp.src('src/components/**/*.html')
+  //  .pipe(htmlMin({collapseWhitespace: true}))
+  //  .pipe(gulp.dest('dist/components'));
+//});
 
 gulp.task('js', function(){
   gulp.src(['src/js/**/*.js', '!src/js/vendor/*.js'])
     //.pipe(concat('bundle.js'))
-    .pipe(uglify())
+    .pipe(uglify(/*{mangle: false}*/))
     .pipe(gulp.dest('dist/js'));
 });
 
+//gulp.task('componentsJs', function(){
+  //gulp.src(['src/components/**/*.js'])
+    //.pipe(concat('bundle.js')) NO QUITAR ESTE A MENOS QUE SE USE
+    //.pipe(uglify({mangle: false}))
+    //.pipe(gulp.dest('dist/components'));
+//});
+
 gulp.task('img', function(){
-  //gulp.src(['src/img/**/*.png', 'src/img/**/*.jpg', 'src/img/**/*.gif'])
   gulp.src(['src/img/**/*'])
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
@@ -46,23 +58,23 @@ gulp.task('mirarCSS', function(){
 
 
 gulp.task('copiar', function(){
-	gulp.src(['src/**/*', '!src/**/*.html', '!src/js/**/*', '!src/css/**/*', '!src/img/**/*'])
+	gulp.src(['src/**/*', '!src/**/*.html', '!src/js/**/*', '!src/css/**/*', '!src/img/**/*', '!src/components/**/*'])
 		.pipe(gulp.dest('dist'));
 
 	gulp.src(['src/js/vendor/**/*'])
 		.pipe(gulp.dest('dist/js/vendor'));
 
   gulp.src(['src/css/vendor/**/*'])
-    .pipe(gulp.dest('dist/css/vendor'))
+    .pipe(gulp.dest('dist/css/vendor'));
 
   gulp.src(['src/css/fonts/**/*'])
-    .pipe(gulp.dest('dist/css/fonts'))
-})
+    .pipe(gulp.dest('dist/css/fonts'));
+});
 
 
 gulp.task('iniciar', function(){
-  //gulp.watch(['src/js/**/*.js', '!src/js/vendor/*.js'], ['js']);
   gulp.watch(['src/css/*.styl', '!src/css/vendor/*.css'], ['mirarCSS']);
 });
 
-gulp.task('build', ['copiar', 'html', 'js', 'img', 'css']);
+gulp.task('build', ['copiar', 'html', 'js', 'img', 'css'/*, 'componentsHtml', 'componentsJs'*/]);
+
